af534dfb7a770f182ef02d1b6d2dfe8a066b4e08
Vandana <av102451293@gmail.com>
2023-05-17 00:24:41 +0530

    PythonMasteryChallenge/VandanaJha/YouTube/DearCoding

---
+++ PythonMasteryChallenge.py
@@ -0,0 +1,52 @@
+DC = int(3.2) 
+print(100*DC)
+
+# Output: 300
+
+
+print(type(1+2j)
+
+# Output: <class 'complex'>
+
+
+Py = b'AT'
+print(Py)
+
+# Output: b'AT'
+
+
+x = 1j
+print(x)
+
+# Output: 1j
+
+
+D,E,A,R = 'Py'
+print(D+E+A+R)
+
+# Output: PyPyPyPy
+
+
+C, O = 'C', 'O'
+print(2*(C + O))
+
+# Output: COCO
+
+
+one = input()  #1
+print(one + one)
+
+# Output: 11
+
+
+#print(1) 
+print(23)
+
+# Output: 23
+
+
+if 10 < 24:
+print('Correct') 
+print('Me')
+
+# Output: Error
